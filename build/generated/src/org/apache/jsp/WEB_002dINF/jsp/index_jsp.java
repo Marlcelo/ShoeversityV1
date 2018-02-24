f0ad989e728403da9c0c601fac97070c00ac54aa
Marl Christian Ricanor <32050672+Marlcelo@users.noreply.github.com>
2018-02-24 11:12:14 +0800

    Start of Project

---
+++ build/generated/src/org/apache/jsp/WEB_002dINF/jsp/index_jsp.java
@@ -0,0 +1,89 @@
+package org.apache.jsp.WEB_002dINF.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+import java.util.Date;
+
+public final class index_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\"\r\n");
+      out.write("    \"http://www.w3.org/TR/html4/loose.dtd\">\r\n");
+      out.write("\r\n");
+      out.write("<html>\r\n");
+      out.write("    <head>\r\n");
+      out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\r\n");
+      out.write("        <title>Welcome to Spring Web MVC project</title>\r\n");
+      out.write("    </head>\r\n");
+      out.write("    ");
+
+        Date date = new Date();
+    
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("    <body>\r\n");
+      out.write("        <p>Hello! This is the default welcome page for a Spring Web MVC project.</p>\r\n");
+      out.write("        <p><i>To display a different welcome page for this project, modify</i>\r\n");
+      out.write("            <tt>index.jsp</tt> <i>, or create your own welcome page then change\r\n");
+      out.write("                the redirection in</i> <tt>redirect.jsp</tt> <i>to point to the new\r\n");
+      out.write("                welcome page and also update the welcome-file setting in</i>\r\n");
+      out.write("            <tt>web.xml</tt>.</p>\r\n");
+      out.write("        <p> Todays date is ");
+      out.print( date );
+      out.write("</p>\r\n");
+      out.write("        ");
+
+        
+      out.write("\r\n");
+      out.write("    </body>\r\n");
+      out.write("</html>\r\n");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
