f5aedb6d5bd6f485958a8eb2e15c22145b73896a
Marl Christian Ricanor <32050672+Marlcelo@users.noreply.github.com>
2018-02-28 00:18:03 +0800

    This update is for the login module of both admin and users
    also, whenever the user inputs wrong credentials, they are redirected back to the login page

--- src/java/servlets/Logout.java
+++ src/java/servlets/Logout.java
@@ -1,6 +1,12 @@
 package servlets;
 
 import java.io.IOException;
+import static java.lang.System.out;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -29,5 +35,27 @@ public class Logout extends HttpServlet {
 		
 		//redirect to home page
 		response.sendRedirect(request.getContextPath() + "/login.jsp");
+                
+                String uname = request.getParameter("uname");
+        String password = request.getParameter("pword");
+		
+//	try{
+//            Class.forName("com.mysql.jdbc.Driver");
+//            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/shoeversity", "root", "");
+//            
+//            Statement stmnt = conn.createStatement();
+//            String query = "SELECT * FROM users WHERE u_username=" + uname + " AND u_password=" + password;
+//			
+//            ResultSet rs = stmnt.executeQuery(query);
+//	  
+//	   while(rs.next()) {
+//		   out.println(rs.getString("u_username"));
+//		   out.println(rs.getString("u_password"));
+//	   }
+//            
+////            out.println("data is inserted");
+//        }catch(ClassNotFoundException | SQLException e){
+//            out.println(e);
+//        }
 	}
 }
